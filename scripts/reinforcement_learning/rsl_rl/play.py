81bc4ad0b460ecda260f59aaa2a346fc3ad21ae3
Caio Paiva <c.colares.paiva@gmail.com>
2026-04-12 19:41:51 -0300

    :monocle_face: Added Success rate measurement for the four default tasks for openarm operating in rsl_rl

--- scripts/reinforcement_learning/rsl_rl/play.py
+++ scripts/reinforcement_learning/rsl_rl/play.py
@@ -88,6 +88,8 @@ from isaaclab_tasks.utils.hydra import hydra_task_config
 
 import openarm.tasks  # noqa: F401
 
+from openarm.tasks.manager_based.openarm_manipulation.unimanual.lift.mdp.terminations import object_is_lifted
+
 # PLACEHOLDER: Extension template (do not remove this comment)
 
 
@@ -128,6 +130,109 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
 
     # create isaac environment
     env = gym.make(args_cli.task, cfg=env_cfg, render_mode="rgb_array" if args_cli.video else None)
+    
+    def get_success_function(task_name):
+        if "Lift" in task_name:
+            return lambda env: object_is_lifted(env, minimal_height=0.05)
+
+        elif "Reach" in task_name and ("Bi" in task_name or "Bimanual" in task_name):
+
+            episode_success = {}
+
+            def success_fn(env):
+                env_id = id(env)
+                
+                if env_id not in episode_success:
+                    episode_success[env_id] = None
+
+                robot = env.unwrapped.scene["robot"]
+                body_names = robot.data.body_names
+
+                left_ee_idx = body_names.index("openarm_left_ee_tcp")
+                right_ee_idx = body_names.index("openarm_right_ee_tcp")
+
+                left_ee_pos = robot.data.body_pos_w[:, left_ee_idx]
+                right_ee_pos = robot.data.body_pos_w[:, right_ee_idx]
+                
+                env_origins = env.scene.env_origins
+
+                left_ee_pos_local = left_ee_pos - env_origins
+                right_ee_pos_local = right_ee_pos - env_origins
+
+                left_target = env.unwrapped.command_manager.get_command("left_ee_pose")[:, :3]
+                right_target = env.unwrapped.command_manager.get_command("right_ee_pose")[:, :3]
+
+                left_error = torch.norm(left_ee_pos_local - left_target, dim=1)
+                right_error = torch.norm(right_ee_pos_local - right_target, dim=1)
+
+                in_range = (left_error < 0.15) & (right_error < 0.15)
+
+                if episode_success[env_id] is None:
+                    episode_success[env_id] = torch.zeros_like(in_range, dtype=torch.bool)
+
+                episode_success[env_id] = episode_success[env_id] | in_range
+                
+                return episode_success[env_id]
+
+            return success_fn
+
+        elif "Reach" in task_name and "Bi" not in task_name:
+            episode_success = {}
+            
+            def reach_unimanual_success_fn(env):
+                env_id = id(env)
+                
+                if env_id not in episode_success:
+                    episode_success[env_id] = None
+                
+                robot = env.unwrapped.scene["robot"]
+                body_names = robot.data.body_names
+
+                ee_idx = body_names.index("openarm_ee_tcp")
+
+                ee_pos = robot.data.body_pos_w[:, ee_idx]
+                env_origins = env.scene.env_origins
+                ee_pos_local = ee_pos - env_origins
+
+                target = env.unwrapped.command_manager.get_command("ee_pose")[:, :3]
+
+                error = torch.norm(ee_pos_local - target, dim=1)
+                success_now = error < 0.1
+
+                if episode_success[env_id] is None:
+                    episode_success[env_id] = torch.zeros_like(success_now, dtype=torch.bool)
+
+                episode_success[env_id] |= success_now
+
+                return episode_success[env_id]
+            return reach_unimanual_success_fn
+
+        elif "Drawer" in task_name:
+            episode_success = {}
+            
+            def drawer_success_fn(env):
+                env_id = id(env)
+                
+                if env_id not in episode_success:
+                    episode_success[env_id] = None
+                
+                cabinet = env.unwrapped.scene["cabinet"]
+
+                joint_idx = cabinet.data.joint_names.index("drawer_bottom_joint")
+                joint_pos = cabinet.data.joint_pos[:, joint_idx]
+
+                success_now = joint_pos > 0.35
+
+                if episode_success[env_id] is None:
+                    episode_success[env_id] = torch.zeros_like(success_now, dtype=torch.bool)
+
+                episode_success[env_id] |= success_now
+
+                return episode_success[env_id]          
+            return drawer_success_fn
+        else:
+            raise ValueError(f"Unknown task: {task_name}")
+
 
     # convert to single-agent instance if required by the RL algorithm
     if isinstance(env.unwrapped, DirectMARLEnv):
@@ -185,9 +290,14 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
 
     dt = env.unwrapped.step_dt
 
+    success_fn = get_success_function(args_cli.task)
+
     # reset environment
     obs = env.get_observations()
     timestep = 0
+    successful_episodes = 0
+    total_episodes = 0
+    max_episodes = 250  # pode ajustar
     # simulate environment
     while simulation_app.is_running():
         start_time = time.time()
@@ -196,7 +306,24 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
             # agent stepping
             actions = policy(obs)
             # env stepping
-            obs, _, _, _ = env.step(actions)
+            obs, _, dones, _ = env.step(actions)
+
+        success = success_fn(env.unwrapped)
+
+        if not torch.is_tensor(success):
+            success = torch.tensor(success, device=env.unwrapped.device)
+
+        done_envs = dones.nonzero(as_tuple=True)[0]
+
+        if len(done_envs) > 0:
+            successful_episodes += success[done_envs].sum().item()
+            total_episodes += len(done_envs)
+
+            print(f"Partial Success Rate: {successful_episodes}/{total_episodes}")
+
+        if total_episodes >= max_episodes:
+            break
+
         if args_cli.video:
             timestep += 1
             # Exit the play loop after recording one video
@@ -207,6 +334,13 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
         sleep_time = dt - (time.time() - start_time)
         if args_cli.real_time and sleep_time > 0:
             time.sleep(sleep_time)
+
+    if total_episodes > 0:
+        success_rate = successful_episodes / total_episodes
+        print("\n==============================")
+        print(f"Evaluation over {total_episodes} episodes")
+        print(f"Success Rate: {success_rate * 100:.2f}%")
+        print("==============================\n")
 
     # close the simulator
     env.close()
